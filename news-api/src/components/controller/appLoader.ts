a3de16a2cc4452f1ba02a7e1f606d060adaf77ba
Vitaliy Sobolevskiy <SobVitalya@yandex.ru>
2022-12-09 18:09:20 +0700

    feat: add proxy server

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '1c72241c977d4f06b911ca8f6a5b2be6', // получите свой ключ https://newsapi.org/
         });
     }
