06611774f64b882d913a349882150a0fd102beee
Vitaliy Sobolevskiy <SobVitalya@yandex.ru>
2023-01-15 22:09:52 +0700

    feat: add render page

---
+++ async-race/src/views/App.ts
@@ -0,0 +1,18 @@
+import PageSkelet from './pages/PageSkelet';
+import ControllerPage from '../controllers/ControllerPage';
+
+class App {
+  private page: PageSkelet;
+  private pageController: ControllerPage;
+
+  constructor() {
+    this.page = new PageSkelet();
+    this.pageController = new ControllerPage();
+  }
+  async start() {
+    this.page.render();
+    await this.pageController.start();
+  }
+}
+
+export default App;
